5ced9cb5e1a153ba9b4a9985a976c770569b39bb
RobsonBasquill-Lipsc <robson.basquill-lipscomb@insurwave.com>
2025-06-10 21:36:35 +0100

    add options

---
+++ Papyrus.Domain/Models/Client/AiRequestOptionsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Papyrus.Domain.Models.Client;
+
+public record AiRequestOptionsModel
+{
+    [JsonPropertyName("temperature")]
+    public double Tempurature { get; init; } = 0.2;
+    
+    [JsonPropertyName("top_p")]
+    public double TopP { get; init; } = 0.8;
+}
